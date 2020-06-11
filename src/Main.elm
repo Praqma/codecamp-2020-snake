e8aa55d1a0df7df768fdac46dbaa22325a064f87
Samuel Ytterbrink <neppord@gmail.com>
2020-06-11 15:45:54 +0200

    Add bigger skeleton of the application, to reduce creation of boiler plate in the mob.

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,40 @@
 module Main exposing (..)
 
+import Browser
 import Html exposing (..)
 
-main = text "Hello World"
+
+type alias Model =
+    ()
+
+
+type alias Msg =
+    Never
+
+
+main =
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init flags =
+    ( (), Cmd.none )
+
+
+view model =
+    { title = "Hello World!"
+    , body = [ text "Hello World!" ]
+    }
+
+
+update msg model =
+    ( model, Cmd.none )
+
+
+subscriptions model =
+    Sub.none
